ccc9421097f2e128f4d161d54a3dce61ac39f57a
Batuhan Özel <72559805+batuhanozel20@users.noreply.github.com>
2022-03-07 19:15:25 +0300

    Driving Mechanic Imrovements

--- deathRace/Assets/MovementScript.cs
+++ deathRace/Assets/MovementScript.cs
@@ -4,15 +4,23 @@ public class MovementScript : MonoBehaviour
 {
   public float speed;
   public Rigidbody rb;
+  //public Transform from;
+  //public Transform to;
+  //private float timeCount = 0.0f;
+  //public static Quaternion Slerp(Quaternion a, Quaternion b, float t){}
+  
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
+        
     }
      void Update () {
          Vector3 pos = transform.position;
         var fwd= transform.forward*speed;
         var rgt= transform.right*speed;
+      
 
          if (Input.GetKey ("w")) {
             
@@ -31,17 +39,24 @@ public class MovementScript : MonoBehaviour
    
          if (Input.GetKey ("d")) {
              if(rb.velocity.z>0.05 || rb.velocity.z<-0.05 ){
-            //pos.x += speed * Time.deltaTime;
-
-             //rb.AddForce(rgt/(5/2));
+            
+            // pos.x += speed * Time.deltaTime;
+             rb.AddForce(rgt/(5/2));
              transform.Rotate(0,2*speed*speed*Time.deltaTime,0,Space.Self);
+             
+             //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
+             //timeCount = timeCount + Time.deltaTime;
+            
               }
          }
          if (Input.GetKey ("a")) {
                if(rb.velocity.z<-0.05 || rb.velocity.z>0.05){
              //pos.x -= speed * Time.deltaTime;
-             //rb.AddForce(-rgt/(5/2));
+             rb.AddForce(-rgt/(5/2));
              transform.Rotate(0,-2*speed*speed*Time.deltaTime,0,Space.Self);
+
+             //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
+             //timeCount = timeCount + Time.deltaTime;
              }
          }
              
