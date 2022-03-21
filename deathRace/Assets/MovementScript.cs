3133975c158d20eadb4b111fb3fc82c8c66ed6de
Batuhan Özel <72559805+batuhanozel20@users.noreply.github.com>
2022-03-21 14:31:29 +0300

    İmproved Car Driving Mechanics
    Finally managed to improve after rough 2 weeks

--- deathRace/Assets/MovementScript.cs
+++ deathRace/Assets/MovementScript.cs
@@ -1,68 +1,30 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 public class MovementScript : MonoBehaviour
 {
-  public float speed;
-  public Rigidbody rb;
-  //public Transform from;
-  //public Transform to;
-  //private float timeCount = 0.0f;
-  //public static Quaternion Slerp(Quaternion a, Quaternion b, float t){}
-  
-
-    void Start()
+    public Rigidbody rb;
+    public float speed;
+    public float rotationSpeed;
+    
+     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+       rb = GetComponent<Rigidbody>();
+       }
+
+    void Update()
+    {
+       
+        float translation = Input.GetAxis("Vertical") * speed;
         
-        
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+         if(rb.velocity.z>0.05 || rb.velocity.z<-0.05 ){
+                
+            }
     }
-     void Update () {
-         Vector3 pos = transform.position;
-        var fwd= transform.forward*speed;
-        var rgt= transform.right*speed;
-      
-
-         if (Input.GetKey ("w")) {
-            
-            for (int i = 1; i <=speed/(5/2); i++){
-            rb.AddForce(fwd/(5/2));
-            }
-             
-         }
-         if (Input.GetKey ("s")) {
-           
-           for (int i = 1; i <=speed/(5/2); i++){
-            rb.AddForce(-fwd/(5/2));
-            }
-             
-         }
-   
-         if (Input.GetKey ("d")) {
-             if(rb.velocity.z>0.05 || rb.velocity.z<-0.05 ){
-            
-            // pos.x += speed * Time.deltaTime;
-             rb.AddForce(rgt/(5/2));
-             transform.Rotate(0,2*speed*speed*Time.deltaTime,0,Space.Self);
-             
-             //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
-             //timeCount = timeCount + Time.deltaTime;
-            
-              }
-         }
-         if (Input.GetKey ("a")) {
-               if(rb.velocity.z<-0.05 || rb.velocity.z>0.05){
-             //pos.x -= speed * Time.deltaTime;
-             rb.AddForce(-rgt/(5/2));
-             transform.Rotate(0,-2*speed*speed*Time.deltaTime,0,Space.Self);
-
-             //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, timeCount);
-             //timeCount = timeCount + Time.deltaTime;
-             }
-         }
-             
- 
-         transform.position = pos;
-         
-         
-     }
 }
